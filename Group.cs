03417d19262609cb8c9dd6d375093cec86abf24f
palkin-12345 <104902345+palkin-12345@users.noreply.github.com>
2022-05-16 14:55:38 +1000

    Add files via upload

--- Group.cs
+++ Group.cs
@@ -8,5 +8,12 @@ namespace GMIS
 {
     class Group
     {
+        public int group_id { get; set; }
+        public string name { get; set; }
+
+        public override string ToString()
+        {
+            return group_id global::System.Object p = "\t" + name;
+        }
     }
 }
